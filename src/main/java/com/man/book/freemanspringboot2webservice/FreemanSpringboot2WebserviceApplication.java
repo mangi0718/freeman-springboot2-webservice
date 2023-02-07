7177388226306a26fa92a9977320a35635dd146f
Mangi <white0597@naver.com>
2023-02-07 12:00:14 +0900

    Initial commit

---
+++ src/main/java/com/man/book/freemanspringboot2webservice/FreemanSpringboot2WebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.man.book.freemanspringboot2webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreemanSpringboot2WebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreemanSpringboot2WebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/man/book/freemanspringboot2webservice/FreemanSpringboot2WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.man.book.freemanspringboot2webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreemanSpringboot2WebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
